497b45aa4008d766d50d7cb32283ab5c4bee44a6
Lafertd <43250553+Lafertd@users.noreply.github.com>
2025-01-10 04:59:26 +0100

    Create a friendly Json-Response app.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, redirect
 from config import app
 from v1.core.habit import register_habit_blueprint
 
-@app.route("/docs", methods=['GET'], endpoint="docs", strict_slashes=False)
+app.route("/docs", methods=['GET'], endpoint="docs", strict_slashes=False)
 @app.route("/home", methods=['GET'], endpoint="home", strict_slashes=False)
 @app.route("/", methods=['GET'], endpoint="/", strict_slashes=False)
-
 def home():
     # Redirect to Postman documentation URL
-    return redirect("https://documenter.getpostman.com/view/40761275/2sAYQUotVp")
+    return redirect("https://documenter.getpostman.com/view/40761275/2sAYQUotVp"), jsonify({
+        "status": "success",
+        "message": "Documentation page is returned successfully.",
+        "redirect_url": "https://documenter.getpostman.com/view/40761275/2sAYQUotVp"
+    })
+
 if __name__ == "__main__":
     app.run()
 
